329f8079959fb4734d4e7ca7e393be1dcde73189
Vasant Sachdewa <vasant@Vasants-MacBook-Air.local>
2021-07-03 16:51:15 +0700

    Fixed test for postgres module

--- worker/tests_worker/adapters/test_postgres_adapter.py
+++ worker/tests_worker/adapters/test_postgres_adapter.py
@@ -26,32 +26,36 @@ class TestPostgresAdapter(unittest.TestCase):
 
 	@patch('worker.adapters.postgres_adapter.JobsDetail')
 	def test_store_extracted_data_valid(self, jobs_detail_model): 
-		job_detail_object = MagicMock()
-		jobs_detail_model.return_value = job_detail_object
+		job_detail_obj = MagicMock()
+		job_detail_obj.job_id = 'random_id'
+		update_or_create_fn = MagicMock(return_value=(job_detail_obj, 'irrelevant'))
+		objects_attr = MagicMock()
+		objects_attr.update_or_create = update_or_create_fn
+		jobs_detail_model.objects = objects_attr
 		vendor_obj = Vendors(name='vasant', url='https://www.vasant.com')
 		extracted_data = JOB_DETAIL_DATA
 		adapter = PostgresAdapter()
 		adapter.store_extracted_data(vendor_obj, extracted_data)
-		self.assertTrue(jobs_detail_model.called_with(extracted_data))
-		self.assertTrue(job_detail_object.save.called)
+		self.assertTrue(update_or_create_fn.called_with(extracted_data))
 
 	@patch('worker.adapters.postgres_adapter.JobsRaw')
 	def test_store_raw_data_valid(self, job_raw_model):
 		vendor_obj = Vendors(name='vasant', url='https://www.vasant.com')
 		JOB_DETAIL_DATA['vendor_id'] = vendor_obj
 		job_detail_obj = JobsDetail(**JOB_DETAIL_DATA)
-		raw_data = {'data': 'all the fields from raw'}
-		raw_obj = MagicMock()
-		job_raw_model.return_value = raw_obj
+		raw_data = {'data': 'All fields from raw data'}
+		update_or_create_fn = MagicMock(return_value=('tup_1', 'tup_2'))
+		objects_attr = MagicMock()
+		objects_attr.update_or_create = update_or_create_fn
+		job_raw_model.objects = objects_attr
 		adapter = PostgresAdapter()
 		adapter.store_raw_data(job_detail_obj, raw_data)
-		self.assertTrue(job_raw_model.called_with(
+		self.assertTrue(update_or_create_fn.called_with(
 			id=job_detail_obj,
 			vendor_id=vendor_obj,
 			job_id=job_detail_obj.job_id,
 			raw_data=raw_data
 		))
-		self.assertTrue(raw_obj.save.called)
 
 
 
