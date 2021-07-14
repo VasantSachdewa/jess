c5fb6d2d3f45c7479058f279d2397000ae1917c4
Vasant Sachdewa <vasant@Vasants-MacBook-Air.local>
2021-07-14 12:32:08 +0700

    log message out to std_out when in debug mode

--- jess/libs/logs.py
+++ jess/libs/logs.py
@@ -1,5 +1,6 @@
 import logging
-from jess.settings import LOG_LEVEL_ENV
+import sys
+from jess.settings import LOG_LEVEL_ENV, ENV
 
 LOG_LEVEL = getattr(logging, LOG_LEVEL_ENV)
 
@@ -8,6 +9,7 @@ class Logs:
 
     __instance = None
     log_level = LOG_LEVEL
+    env = ENV
 
     def __init__(self, logger_name: str = "SCRAPER") -> None:
         self.logger = self._get_logger_object(logger_name)
@@ -22,5 +24,12 @@ class Logs:
     def _get_logger_object(self, logger_name: str) -> logging.Logger:
         logger = logging.getLogger(logger_name)
         logger.setLevel(self.log_level)
+        if self.env == 'development':
+            #add sys_out handler
+            self._setup_sys_out(logger)
 
         return logger
+
+    def _setup_sys_out(self, logger: logging.Logger) -> None:
+        sys_out_handler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(sys_out_handler)
