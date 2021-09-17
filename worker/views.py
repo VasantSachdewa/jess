7d085f65d4b7ad7cd4553a04cf5e27ce39ea6c69
Vasant Sachdewa <vasant@Vasants-MacBook-Air.local>
2021-09-17 21:40:28 +0700

    Added pagination functionality

--- worker/views.py
+++ worker/views.py
@@ -1,9 +1,17 @@
+from django.core.paginator import Page
 from worker.models import JobsDetail
-from rest_framework import viewsets
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.pagination import PageNumberPagination
 from worker.validators import JobsDetailSerializer
 
 
+class StandardPaginationConfig(PageNumberPagination):
+    page_size_query_param = 'pageSize'
+    max_page_size = 10
+
+
 # Create your views here.
-class JobDetailViewSet(viewsets.ReadOnlyModelViewSet):
+class JobDetailViewSet(ReadOnlyModelViewSet):
     queryset = JobsDetail.objects.all()
     serializer_class = JobsDetailSerializer
+    pagination_class = StandardPaginationConfig
