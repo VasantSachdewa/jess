3b5e99f6f7fecd9f23c4be9815c98ed0ce7f5434
Vasant Sachdewa <vasant@Vasants-MacBook-Air.local>
2021-09-22 15:00:22 +0700

    Added code to time functiions

--- jess/libs/metrics_tracker.py
+++ jess/libs/metrics_tracker.py
@@ -5,6 +5,7 @@ from typing import Callable
 from jess.exceptions import MetricException
 
 import boto3
+import time
 from jess.settings import CLOUDWATCH_NAMESPACE
 
 
@@ -41,5 +42,43 @@ def counter(metric_name: str) -> Callable:
                 pass  
             
         return implementation 
+            
+    return wrapper
+
+
+def timer(metric_name: str) -> Callable:
+    
+    def wrapper(func: Callable) -> Callable:
+        
+        @wraps(func)
+        def implementation(*args, **kwargs):
+            time1 = time.time()
+            output = func(*args, **kwargs)
+            time2 = time.time()
+            elasped_time = round((time2 - time1)*1000, 3)
+            _timer(elasped_time)
+
+            return output
+            
+        def _timer(elasped_time: float):
+            try:
+                response = CLOUDWATCH_CLIENT.put_metric_data(
+                    MetricData = [
+                        {
+                            'MetricName': '{}-{}'.format(metric_name, 'timer'),
+                            'Value': elasped_time,
+                            'StorageResolution': 1,
+                            'Timestamp': datetime.datetime.now(datetime.timezone.utc),
+                            'Unit': 'Milliseconds'
+                        },
+                    ],
+                    Namespace=CLOUDWATCH_NAMESPACE
+                )
+            except Exception as e:
+                raise MetricException(e)
+            except MetricException:
+                pass  
+            
+        return implementation
             
     return wrapper
--- worker/views.py
+++ worker/views.py
@@ -3,7 +3,7 @@ from worker.models import JobsDetail
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.pagination import PageNumberPagination
 from worker.validators import JobsDetailSerializer
-from jess.libs.metrics_tracker import counter
+from jess.libs.metrics_tracker import counter, timer
 
 
 class StandardPaginationConfig(PageNumberPagination):
@@ -17,6 +17,32 @@ class JobDetailViewSet(ReadOnlyModelViewSet):
     serializer_class = JobsDetailSerializer
     pagination_class = StandardPaginationConfig
 
+    @timer('worker.JobDetailViewSet.list')
     @counter('worker.JobDetailViewSet.list')
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+    @timer('worker.JobDetailViewSet.filter_queryset')
+    @counter('worker.JobDetailViewSet.filter_queryset')
+    def filter_queryset(self, queryset):
+        return super().filter_queryset(queryset)
+
+    @timer('worker.JobDetailViewSet.get_queryset')
+    @counter('worker.JobDetailViewSet.get_queryset')
+    def get_queryset(self):
+        return super().get_queryset()
+
+    @timer('worker.JobDetailViewSet.paginate_queryset')
+    @counter('worker.JobDetailViewSet.paginate_queryset')
+    def paginate_queryset(self, queryset):
+        return super().paginate_queryset(queryset)
+
+    @timer('worker.JobDetailViewSet.get_paginated_response')
+    @counter('worker.JobDetailViewSet.get_paginated_response')
+    def get_paginated_response(self, data):
+        return super().get_paginated_response(data)
+
+    @timer('worker.JobDetailViewSet.get-serializer')
+    @counter('worker.JobDetailViewSet.get-serializer')
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
